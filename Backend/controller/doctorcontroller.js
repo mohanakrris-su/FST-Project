0c8afbbc8297a8e904502f9e0ac66f5c651eb15e
mohanakrris-su <mohanakrris@gmail.com>
2026-04-03 22:07:19 +0530

    Delete Backend directory

--- Backend/controller/doctorcontroller.js
+++
@@ -1,9 +0,0 @@
-const Doctor=require('../models/Doctor.js')
-async function addDoctors(req,res){
-    try{
-        Doctor.create({name:req.body.name})
-    }
-    catch(err){
-
-    }
-}
--- Backend/routes/departmentrouter.js
+++
@@ -1,5 +0,0 @@
-const router=require('express').Router()
-const {addDepartment,getDepartments}=require('../controller/departmentcontroller.js');
-router.get('/',getDepartments);
-router.post('/',addDepartment);
-module.exports=router;
